6a99771fc7fb5ba1b8cb1a0777c68146c8c06c2d
ConVid-19 <45920023+Nash-Cash@users.noreply.github.com>
2020-03-14 09:30:28 +0100

    -

---
+++ src/config/Ascii.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2018, The TurtleCoin Developers
+// Copyright (c) 2019, The Xenium Developers
+//
+// Please see the included LICENSE file for more information
+
+#pragma once
+
+#include <string>
+
+const std::string asciiArt = "\n";
